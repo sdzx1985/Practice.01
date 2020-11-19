7dfd141ba3d402bd2c4572dcf74a8913c90cf6e7
sdzx1985 <aiden.hyunjae@gmail.com>
2020-11-19 13:03:25 -0500

    Test Assignment #1

--- Project2/Practice.01.cpp
+++ Project2/Practice.01.cpp
@@ -3,5 +3,29 @@ using namespace std;
 int main(void)
 {
 	cout << "Hello World" << endl;
+
+	string a = "H";
+	string b = "e";
+	string c = "l";
+	string d = "o";
+	string e = "w", f = "o", g = "r", h = "d";
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	cout << f << endl;
+	cout << g << endl;
+	cout << c << endl;
+	cout << h << endl;
+
+	string z = a+b+c+c+d+" "+e+f+g+c+h;
+
+	cout << z << endl;
+
+
+
 	return 0;
 	}
